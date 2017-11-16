471b1a1067a034656eb40204220e7bb1da42440e
KRedd32 <dorsey_k17@hotmail.com>
2017-11-15 20:35:47 -0600

    Test objects added to src/test/java. Project Mavenfied

---
+++ src/test/java/mavenTest/ConCatTest.java
@@ -0,0 +1,16 @@
+package mavenTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ConCatTest {
+
+	@Test
+	public void Concattest() {
+		MyJUnitClass junit = new MyJUnitClass();
+		String result = junit.ConCat("Hello ", "World");
+		assertEquals("Hello World", result);
+	}
+
+}
